c2e98ddfabdc3ed3c4d73aa84b4b0952bc6c0175
Gabriel Grazionale <gabriel.grazionale@portalsolar.com>
2020-12-08 09:04:23 -0300

    Login Form Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import MainComponent from '../components/shared/MainComponent'
+import { useRouter } from 'next/router'
 
 const Home: React.FC = () => {
-  return <h1>Hello World</h1>
+  const router = useRouter()
+
+  return (
+    <MainComponent>
+      <h1>Hello World</h1>
+      <button onClick={() => router.push('/Auth/Login')}>Login</button>
+    </MainComponent>
+  )
 }
 
 export default Home
