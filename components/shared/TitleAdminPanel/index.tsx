48fcb1b2d1be7b75fdaebac6a18e99d867eb9ce4
Gabriel Grazionale <gabriel.grazionale@portalsolar.com>
2021-01-15 10:11:13 -0300

    Add newPath to TitleAdminPanel

--- components/shared/TitleAdminPanel/index.tsx
+++ components/shared/TitleAdminPanel/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
-import TitleAndPath from './TitleAndPath';
-import SearchAndIcon from './SearchAndIcon';
 import styles from '../../../styles/AdminTitle.module.css';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import TitleAndPath from './TitleAndPath';
+import SearchAndIcon from './SearchAndIcon';
 
 interface TitleAdminPanelProps {
-  title: String,
-  path: String,
-  icon?: IconProp
+  title: string;
+  path: string;
+  icon?: IconProp;
+  newPath: string;
 }
 
-const TitleAdminPanel: React.FC<TitleAdminPanelProps> = ({ title, path, icon }) => {
+const TitleAdminPanel: React.FC<TitleAdminPanelProps> = ({ title, path, icon, newPath }) => {
   return (
     <Row className="mt-4">
       {
@@ -22,7 +23,7 @@ const TitleAdminPanel: React.FC<TitleAdminPanelProps> = ({ title, path, icon }) 
             </Col>
 
             <Col lg={{ span: 4, offset: 2 }} xs={8}>
-              <SearchAndIcon icon={icon} />
+              <SearchAndIcon icon={icon} newPath={newPath} />
             </Col>
           </>
           :
