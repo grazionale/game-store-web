f59e6ad7cf0a7431acd17dc99ce7e39d0da6ad1d
Gabriel Grazionale <gabriel.grazionale@portalsolar.com>
2021-02-25 12:15:17 -0300

    Improve homepage with new components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,44 @@
 import MainComponent from '../components/shared/MainComponent';
+import { Carousel } from 'react-bootstrap';
+import styles from './styles.module.css';
+import HighlightedProducts from '../components/Storefront/HighlightedProducts';
 
-const Home: React.FC = () => {
+const Storefront: React.FC = () => {
   return (
     <MainComponent>
-      <h1>Home</h1>
+      <Carousel className={styles.carousel}>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://meups.com.br/wp-content/uploads/2018/01/God-of-War-4-900x503.jpg"
+            alt="First slide"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://meups.com.br/wp-content/uploads/2018/01/God-of-War-4-900x503.jpg"
+            alt="Second slide"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://meups.com.br/wp-content/uploads/2018/01/God-of-War-4-900x503.jpg"
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+
+      <HighlightedProducts title="Ofertas da Semana" type="highlighted" />
+
+      <HighlightedProducts title="Lançamentos" />
+
+      <HighlightedProducts title="Mais Populares" />
     </MainComponent>
   )
 }
 
-export default Home;
+export default Storefront;
