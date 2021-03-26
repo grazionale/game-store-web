1481f4defa38a895347480a274825011e495199b
Gabriel Grazionale <gabriel.grazionale@portalsolar.com>
2021-03-26 09:19:31 -0300

    Using ProductSearchService in StoreFrontHeader component

--- components/shared/Header/StorefrontHeader/index.tsx
+++ components/shared/Header/StorefrontHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { InputGroup, FormControl, Row, Col } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
@@ -6,12 +6,24 @@ import {
   faShoppingCart,
   faUserCircle
 } from '@fortawesome/free-solid-svg-icons'
+import ProductSearchService from '../../../../util/ProductSearchService';
+import { useRouter } from 'next/router';
 
 import Logo from '../../Logo'
 import styles from './styles.module.css';
 import Link from 'next/link'
 
 const StorefrontHeader: React.FC = () => {
+  const [search, setSearch] = useState('');
+
+  const router = useRouter();
+
+  const handleSearch = (): void => {
+    router.push(`
+      /Search${ProductSearchService.execute({ search })}
+    `);
+  }
+
   return (
     <Row className={styles.background}>
       <Col md={6} className="mt-2">
@@ -21,17 +33,34 @@ const StorefrontHeader: React.FC = () => {
       <Col md={6} className="mt-2 text-center">
         <Row>
           <Col md={6} className="mb-4 mb-md-0">
-            <InputGroup>
-              <FormControl placeholder="Pesquisar produto" />
+            <InputGroup className={`${router.pathname === '/Search' ? styles.hidden : ''}`}>
+              <FormControl
+                placeholder="Pesquisar produto"
+                value={search}
+                onChange={
+                  (evt: React.ChangeEvent<HTMLInputElement>) =>
+                    setSearch(evt.target.value)
+                }
+
+                onKeyUp={
+                  (evt: React.KeyboardEvent<HTMLInputElement>) => {
+                    if (evt.key.toLowerCase() === 'enter') {
+                      handleSearch();
+                    }
+                  }
+                }
+              />
             </InputGroup>
           </Col>
 
           <Col md={6}>
             <Row>
-              <Col md={4} xs={4}>
+              <Col
+                className={`${router.pathname === '/Search' ? styles.hidden : ''}`}>
                 <FontAwesomeIcon
                   icon={faSearch}
                   color="var(--color-gray-light)"
+                  onClick={handleSearch}
                 />
               </Col>
 
